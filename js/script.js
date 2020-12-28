e6e8aaa9a7b6e9b0cbc21b42c0360f8518508253
Andre <example@gmail.com>
2020-12-28 13:42:11 +0200

    My first practise

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 "use strict";
+let numberOfFilms = prompt("How much film you can see in this time?", "5");
 
-const isChecked = false,
-      isClose = false;
+let a = prompt("Один з останіх переглянутих фільмів?", "logan"),
+    b = prompt("На скільки оціните його?", "5"),
+    c = prompt("Один з останіх переглянутих фільмів?", "logan"),
+    d = prompt("На скільки оціните його?", "5");
 
-console.log(isChecked || !isClose);
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+/* console.log(personalMovieDB.count + " Right");
+console.log(personalMovieDB.movies.answer); */
